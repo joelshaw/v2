e400d1a7b47c38fa7d291c3a16f0a570392a1ae6
Joel Shaw <shawji@me.com>
2023-05-17 17:58:44 -0500

    Created introduction component, refactored exiting code

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Introduction from './components/introduction';
+
+const App = () => {
+  return (
+    <div>
+      <Introduction></Introduction>
+    </div>
+  );
+}
+
+export default App;
