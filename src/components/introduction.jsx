31d32083f4a5fefcdf916e5e690434f70412b1cf
Joel Shaw <shawji@me.com>
2023-07-09 14:58:16 -0500

    Alignment update

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -9,7 +9,7 @@ const StyledIntro = styled.div`
     height: inherit;
     justify-content: center;
     position: absolute;
-    left: 25%;
+    left: 15%;
 `;
 
 const StyledHeadline = styled.h1`
@@ -17,6 +17,12 @@ const StyledHeadline = styled.h1`
     line-height: 5em;
     text-transform: uppercase;
     letter-spacing: 0.15em;
+`;
+
+const StyledSubheadline = styled.h2`
+    font-size: 1.25em;
+    line-height: 2.5em;
+    margin-bottom: 0.5em;
 `;
 
 const LocationArrow = styled(Airplane)`
@@ -31,7 +37,9 @@ const Introduction = () => {
             <StyledHeadline>
                 <h1>Who is Joel Shaw?</h1>
             </StyledHeadline>
-            <h2>A senior software engineer, photographer, and speaker.</h2>
+            <StyledSubheadline>
+                <h2>A senior software engineer, photographer, and speaker.</h2>                
+            </StyledSubheadline>
             <h3>Richmond, VA <LocationArrow /> Nashville, TN</h3>
         </StyledIntro>
     );
